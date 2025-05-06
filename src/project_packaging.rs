b26e3020f5451bc763109e128ef3fb246937b95c
xsy420 <xsy0420@gmail.com>
2025-05-07 06:55:42 +0800

    refactor: add LoopableNumberedEnum derive and refactor code

--- src/project_packaging.rs
+++ src/project_packaging.rs
@@ -1,8 +1,12 @@
+use num_derive::{FromPrimitive, ToPrimitive};
+use num_traits::{FromPrimitive, ToPrimitive};
+use project_setup_derive::LoopableNumberedEnum;
 use std::fmt::Debug;
 use strum::Display;
 
-#[derive(Debug, Clone, Default, Display)]
+#[derive(Debug, Clone, Default, Display, FromPrimitive, ToPrimitive, LoopableNumberedEnum)]
 #[allow(dead_code)]
+#[numbered_enum(loop_within = 7)]
 enum ProjectPackaging {
     #[default]
     NotNeed,
