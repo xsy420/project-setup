e0cac80b387d432b938bd61c74fa90116bc075b7
xsy420 <xsy0420@gmail.com>
2026-01-19 04:31:26 +0800

    refactor: use OnceLock instead of LazyLock (#196)
    `EXECUTABLE_CACHE` won't be used in other threads, and `OnceLock::get` is faster than `LazyLock::lock`

--- src/common/executable.rs
+++ src/common/executable.rs
@@ -1,18 +1,18 @@
 use crate::app::{PreparePermit, PrepareRecv, PrepareTrait};
+use std::collections::HashMap;
 use std::process::Command;
-use std::sync::LazyLock;
-use std::{collections::HashMap, sync::Mutex};
+use std::sync::OnceLock;
 use strum::IntoEnumIterator;
 pub(crate) trait ExecutableEnumTrait {
     fn exe(&self) -> String;
 }
-static EXECUTABLE_CACHE: LazyLock<Mutex<HashMap<String, bool>>> =
-    LazyLock::new(|| Mutex::new(HashMap::new()));
+static EXECUTABLE_CACHE: OnceLock<HashMap<String, bool>> = OnceLock::new();
 pub(crate) struct Executable {}
 impl PrepareTrait for Executable {
     async fn prepare(permit: &mut PreparePermit<'_>, offset: f64) -> bool {
         let cvs: Vec<String> = Self::list(false);
         let mut recv = PrepareRecv::new(offset);
+        let mut cache: HashMap<String, bool> = HashMap::new();
         for ele in cvs {
             let res = ele.is_empty()
                 || Command::new(ele.clone())
@@ -20,9 +20,10 @@ impl PrepareTrait for Executable {
                     .output()
                     .map(|o| o.status.success())
                     .unwrap_or(false);
-            EXECUTABLE_CACHE.lock().unwrap().insert(ele, res);
+            cache.insert(ele, res);
             recv.send_ok(permit);
         }
+        let _ = EXECUTABLE_CACHE.set(cache);
         true
     }
 
@@ -50,6 +51,6 @@ impl Executable {
     }
 
     pub(crate) fn executable(cmd: &str) -> bool {
-        *EXECUTABLE_CACHE.lock().unwrap().get(cmd).unwrap_or(&false)
+        *EXECUTABLE_CACHE.get().unwrap().get(cmd).unwrap_or(&false)
     }
 }
